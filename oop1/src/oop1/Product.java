8af57447ad01864c300dc4a1529e613fb3add54e
Kübra Kılıç <69002604+kubrakll@users.noreply.github.com>
2022-09-22 21:32:22 +0300

    Add files via upload

---
+++ oop1/src/oop1/Product.java
@@ -0,0 +1,42 @@
+package oop1;
+
+public class Product {
+	private String Name;
+	private int Discount;
+	private int UnitPrice;
+	private int UnitsInStock;
+	private String ImageUrl;
+	public String getName() {
+		return Name;
+	}
+	public void setName(String name) {
+		Name = name;
+	}
+	public int getDiscount() {
+		return Discount;
+	}
+	public void setDiscount(int discount) {
+		Discount = discount;
+	}
+	public int getUnitPrice() {
+		return UnitPrice;
+	}
+	public void setUnitPrice(int unitPrice) {
+		UnitPrice = unitPrice;
+	}
+	public int getUnitsInStock() {
+		return UnitsInStock;
+	}
+	public void setUnitsInStock(int unitsInStock) {
+		UnitsInStock = unitsInStock;
+	}
+	public String getImageUrl() {
+		return ImageUrl;
+	}
+	public void setImageUrl(String imageUrl) {
+		ImageUrl = imageUrl;
+	}
+	
+	
+
+}
