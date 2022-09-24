c74bfd46fb663173b84813ebdb0c5b74ef2cdbdb
Kübra Kılıç <69002604+kubrakll@users.noreply.github.com>
2022-09-25 00:08:41 +0300

    21.09.22

---
+++ oop1/src/oop1/CorporateCustomer.java
@@ -0,0 +1,22 @@
+package oop1;
+
+public class CorporateCustomer extends Customer {
+	private String CompanyName;
+	private String TaxNumber;
+
+	public String getCompanyName() {
+		return CompanyName;
+	}
+
+	public void setCompanyName(String companyName) {
+		CompanyName = companyName;
+	}
+
+	public String getTaxNumber() {
+		return TaxNumber;
+	}
+
+	public void setTaxNumber(String taxNumber) {
+		TaxNumber = taxNumber;
+	}
+}
